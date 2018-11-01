c650609d362545983a81ca2eaa59f567a9ce77c1
Skullbonk <reagan.fairbanks@gmail.com>
2018-11-01 11:49:52 -0600

    Initial commit

---
+++ MakeStuffApp/Controller/MakeStuffViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MakeStuffApp
+//
+//  Created by Fairbanks, Reagan on 11/1/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
